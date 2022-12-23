c9ee627f3acbdff294669f7ec8cdeb5edc77a6b2
Kali <thezentester@gmail.com>
2022-12-23 06:50:30 -0800

    updating setup script

--- setup.sh
+++ setup.sh
@@ -5,9 +5,10 @@
 ###  Source: https://github.com/charmbracelet/glow
 ### Install Instructions: https://snapcraft.io/install/glow/debian
 if ! [ -x "$(command -v glow)" ]; then
-  echo 'Error: glow is not installed.' >&2
+  echo 'glow is not installed. Installing glow now...' >&2
   sudo apt update
   sudo apt install snapd
+  sudo service snapd start
   sudo snap install core
   sudo snap install glow
 fi
@@ -17,3 +18,5 @@ sudo cp -r $(pwd) /usr/local/bin/
 
 # creat symlink for `/usr/local/bin/enumnotes.sh` --> /usr/local/bin/enumnotes/enumnotes.sh
 sudo ln -s /usr/local/bin/enumnotes/enumnotes.sh /usr/local/bin/enumnotes.sh 
+
+sudo updatedb
