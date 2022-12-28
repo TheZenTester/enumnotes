363d4a790b388961ba3b26bd5c3d53d39c7e01c8
Kali <thezentester@gmail.com>
2022-12-28 09:27:05 -0800

    updating setup script

--- setup.sh
+++ setup.sh
@@ -7,13 +7,14 @@
 if ! [ -x "$(command -v glow)" ]; then
   echo 'glow is not installed. Installing glow now...' >&2
   echo 'getting binary...' >&2
-  wget https://github.com/charmbracelet/glow/releases/download/v1.4.1/glow_1.4.1_linux_amd64.deb ~/Downloads
+  wget https://github.com/charmbracelet/glow/releases/download/v1.4.1/glow_1.4.1_linux_amd64.deb -P ~/Downloads
   chmod 754 ~/Downloads/glow_1.4.1_linux_amd64.deb
   sudo apt install ~/Downloads/glow_1.4.1_linux_amd64.deb
 fi
 
 # Copy all files from github repo to directory
 sudo cp -r $(pwd) /usr/local/bin/
+sudo chown -R $USER /usr/local/bin/enumnotes
 
 # creat symlink for `/usr/local/bin/enumnotes.sh` --> /usr/local/bin/enumnotes/enumnotes.sh
 sudo ln -s /usr/local/bin/enumnotes/enumnotes.sh /usr/local/bin/enumnotes.sh 
